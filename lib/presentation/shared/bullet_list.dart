1adacceece856064a945aa5cb4724288e296d71c
Efrain <mimo4325@gmail.com>
2022-06-05 14:16:45 -0500

    [Presentation] Added a few shared widgets

---
+++ lib/presentation/shared/bullet_list.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:my_expenses/domain/extensions/iterable_extensions.dart';
+import 'package:my_expenses/presentation/shared/styles.dart';
+
+class BulletList extends StatelessWidget {
+  final List<String> items;
+  final IconData icon;
+  final double iconSize;
+  final Widget Function(int)? iconResolver;
+  final double fontSize;
+  final Function(int)? onDelete;
+  final EdgeInsets padding;
+  final bool addTooltip;
+
+  const BulletList({
+    Key? key,
+    required this.items,
+    this.icon = Icons.fiber_manual_record,
+    this.iconSize = 15,
+    this.iconResolver,
+    this.fontSize = 11,
+    this.onDelete,
+    this.padding = Styles.edgeInsetAll5,
+    this.addTooltip = true,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: items
+          .mapIndex(
+            (e, index) => _ListItem(
+              index: index,
+              title: e,
+              icon: icon,
+              fontSize: fontSize,
+              iconSize: iconSize,
+              iconResolver: iconResolver,
+              onDelete: onDelete,
+              padding: padding,
+              addTooltip: addTooltip,
+            ),
+          )
+          .toList(),
+    );
+  }
+}
+
+class _ListItem extends StatelessWidget {
+  final int index;
+  final String title;
+  final IconData icon;
+  final double iconSize;
+  final Widget Function(int)? iconResolver;
+  final double fontSize;
+  final Function(int)? onDelete;
+  final EdgeInsets padding;
+  final bool addTooltip;
+
+  const _ListItem({
+    Key? key,
+    required this.index,
+    required this.title,
+    required this.icon,
+    required this.iconSize,
+    this.iconResolver,
+    required this.fontSize,
+    this.onDelete,
+    required this.padding,
+    required this.addTooltip,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Padding(
+      padding: padding,
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          if (iconResolver != null) iconResolver!(index) else Icon(icon, size: iconSize),
+          Expanded(
+            child: addTooltip
+                ? Tooltip(
+                    message: title,
+                    child: Container(
+                      margin: const EdgeInsets.only(left: 5),
+                      child: Text(
+                        title,
+                        style: theme.textTheme.bodyText2!.copyWith(fontSize: fontSize),
+                      ),
+                    ),
+                  )
+                : Container(
+                    margin: const EdgeInsets.only(left: 5),
+                    child: Text(
+                      title,
+                      style: theme.textTheme.bodyText2!.copyWith(fontSize: fontSize),
+                    ),
+                  ),
+          ),
+          if (onDelete != null)
+            InkWell(
+              customBorder: const CircleBorder(),
+              child: Icon(Icons.delete, size: iconSize),
+              onTap: () => onDelete!(index),
+            ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/presentation/shared/dialogs/info_dialog.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:my_expenses/generated/l10n.dart';
+import 'package:my_expenses/presentation/shared/bullet_list.dart';
+import 'package:my_expenses/presentation/shared/extensions/media_query_extensions.dart';
+
+class InfoDialog extends StatelessWidget {
+  final List<String> explanations;
+
+  const InfoDialog({
+    Key? key,
+    required this.explanations,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final s = S.of(context);
+    return AlertDialog(
+      title: Text(s.information),
+      content: SingleChildScrollView(
+        child: SizedBox(
+          width: MediaQuery.of(context).getWidthForDialogs(),
+          child: BulletList(items: explanations, fontSize: 14),
+        ),
+      ),
+      actions: [
+        ElevatedButton(
+          onPressed: () => Navigator.of(context).pop(),
+          child: Text(s.ok),
+        )
+      ],
+    );
+  }
+}
