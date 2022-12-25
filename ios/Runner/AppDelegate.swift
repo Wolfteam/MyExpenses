71c3fc70f4e42e2a3c8ac90a9fb7e048269128a7
Efrain Bastidas <mimo4325@gmail.com>
2022-12-25 14:41:26 -0500

    [ios] Added missing workmanager code

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -5,17 +5,36 @@ import workmanager
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    if #available(iOS 10.0, *) {
-      UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
+    /// Registers all pubspec-referenced Flutter plugins in the given registry.
+    static func registerPlugins(with registry: FlutterPluginRegistry) {
+        GeneratedPluginRegistrant.register(with: registry)
     }
     
-    GeneratedPluginRegistrant.register(with: self)
-    //WorkmanagerPlugin.registerTask(withIdentifier: "my_expenses_sync_task")
-    //WorkmanagerPlugin.registerTask(withIdentifier: "my_expenses_recurring_trans_task")
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        if #available(iOS 10.0, *) {
+            UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
+        }
+        //Each 30 min
+        UIApplication.shared.setMinimumBackgroundFetchInterval(60 * 30)
+        WorkmanagerPlugin.registerTask(withIdentifier: "my_expenses_sync_task")
+        WorkmanagerPlugin.registerTask(withIdentifier: "my_expenses_recurring_trans_task")
+        
+        AppDelegate.registerPlugins(with: self) // Register the app's plugins in the context of a normal run
+        
+        WorkmanagerPlugin.setPluginRegistrantCallback { registry in
+            // The following code will be called upon WorkmanagerPlugin's registration.
+            // Note : all of the app's plugins may not be required in this context ;
+            // instead of using GeneratedPluginRegistrant.register(with: registry),
+            // you may want to register only specific plugins.
+            AppDelegate.registerPlugins(with: registry)
+        }
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+    
+    override func applicationDidEnterBackground(_ application: UIApplication) {
+        super.applicationDidEnterBackground(application)
+    }
 }
