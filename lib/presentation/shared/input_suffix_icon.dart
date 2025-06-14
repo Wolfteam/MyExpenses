d3f0ec2263e5147af71d196e4f25c810f9f383f7
Efrain Bastidas <mimo4325@gmail.com>
2025-06-13 23:53:54 -0500

    Removed old nil lib

--- lib/presentation/shared/input_suffix_icon.dart
+++ lib/presentation/shared/input_suffix_icon.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:my_expenses/domain/extensions/string_extensions.dart';
-import 'package:nil/nil.dart';
 
 class InputSuffixIcon extends StatelessWidget {
   final TextEditingController controller;
@@ -16,10 +15,10 @@ class InputSuffixIcon extends StatelessWidget {
   Widget build(BuildContext context) {
     return !controller.text.isNullEmptyOrWhitespace && focusNode.hasFocus
         ? IconButton(
-            alignment: Alignment.bottomCenter,
-            icon: const Icon(Icons.close),
-            onPressed: () => controller.clear(),
-          )
-        : nil;
+          alignment: Alignment.bottomCenter,
+          icon: const Icon(Icons.close),
+          onPressed: () => controller.clear(),
+        )
+        : const SizedBox.shrink();
   }
 }
