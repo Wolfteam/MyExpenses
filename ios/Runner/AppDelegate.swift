655150af5f790f46669a2a9ade08e84abeff21f2
Efrain Bastidas <mimo4325@gmail.com>
2025-12-27 18:21:56 -0500

    [iOS] Updated work manager logic

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,7 +1,6 @@
 import UIKit
 import Flutter
-import workmanager
-
+import workmanager_apple
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -17,24 +16,26 @@ import workmanager
         if #available(iOS 10.0, *) {
             UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
         }
-        //Each 720 min aka 12 hrs
-        UIApplication.shared.setMinimumBackgroundFetchInterval(60 * 720)
-        WorkmanagerPlugin.registerTask(withIdentifier: "my_expenses_sync_task")
-        WorkmanagerPlugin.registerTask(withIdentifier: "my_expenses_recurring_trans_task")
         
         AppDelegate.registerPlugins(with: self) // Register the app's plugins in the context of a normal run
-        
-        WorkmanagerPlugin.setPluginRegistrantCallback { registry in
-            // The following code will be called upon WorkmanagerPlugin's registration.
-            // Note : all of the app's plugins may not be required in this context ;
-            // instead of using GeneratedPluginRegistrant.register(with: registry),
-            // you may want to register only specific plugins.
-            AppDelegate.registerPlugins(with: registry)
-        }
         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
     
     override func applicationDidEnterBackground(_ application: UIApplication) {
         super.applicationDidEnterBackground(application)
+    }
+    
+    override func applicationDidFinishLaunching(_ application: UIApplication) {
+        //each hour
+        let frequency = NSNumber(value: 60 * 60)
+        WorkmanagerPlugin.registerPeriodicTask(
+            withIdentifier: "my_expenses_sync_task",
+            frequency: frequency
+        )
+    
+        WorkmanagerPlugin.registerPeriodicTask(
+            withIdentifier: "my_expenses_recurring_trans_task",
+            frequency: frequency
+        )
     }
 }
